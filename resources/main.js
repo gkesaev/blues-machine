d282ce420cc44d2c79adfee40bda67a4a40154bc
Sergey Elishaev <selishae@campus.haifa.ac.il>
2018-07-18 22:34:50 +0300

    styling of the main page

--- resources/main.js
+++ resources/main.js
@@ -1,4 +1,24 @@
+//import { WSAVERNOTSUPPORTED } from "constants";
+
+var buffer = [];
+
 function playNote(note){
     var note = document.querySelector('.' + note);
     note.play();
+}
+
+function saveNote(note, buffer){
+    //save the selected note to buffer on click
+}
+
+function storeSequence(buffer){
+    //save the sequence to the DB
+}
+
+function addDuplicate(buffer){
+    //add a duplicate of the sequence to the DB
+}
+
+function addRevesed(buffer){
+    //add a revesed duplicate to the DB
 }
