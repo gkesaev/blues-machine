2433d4a2d0d1eb20495cd0da3a99c454b5badb10
GeorgeKesaev <kesaev@gmail.com>
2018-08-21 20:47:17 +0300

    serve index

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ let moment = require('moment');
 let express = require('express');
 let logRequest = require('log-request');
 
+let path = require("path");
+
 let app = express();
 console.log(__dirname);
 app.use(express.static('client'));
@@ -12,22 +14,47 @@ app.get('/api/moment', logRequest, (req, res) => {
 app.get('/the-answer', logRequest, (req, res) => {
     res.send(String(42));
 });
+app.get("/", (req, res) => {
+    console.log("huynayaaaaaaaa");
+    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/resources/style.css'));
+    // res.sendFile(path.join(__dirname + '/resources/main.js'));
+})
 
-app.listen(8000);
+app.get("/resources/main.js", (req, res) => {
+    console.log("serve js");
+    res.sendFile(path.join(__dirname + "/resources/main.js"));
+})
 
-function onRequest(request, response) {
-    let urlObj = url.parse(request.url);
+app.get("/resources/style.css", (req, res) => {
+    console.log("serve css");
+    res.sendFile(path.join(__dirname + "/resources/style.css"));
+})
 
-    if (urlObj.pathname === '/api/moment') {
-        let params = qs.parse(urlObj.query);
-        handleMoment(request, response, params);
-        return;
-    }
+let port = 8000
+let address = '0.0.0.0'
+app.listen(port, address, () => {
+    console.log("server listening on: http://" + address + ":" + port)
+});
 
-    getFile(urlObj.pathname)
-        .then(data => handleFile(request, response, data))
-        .catch(err => handleFileError(request, response, err))
-}
+// function onRequest(request, response) {
+//     console.log("handling request");
+//     let urlObj = url.parse(request.url);
+
+//     if (urlObj.pathname === '/api/moment') {
+//         let params = qs.parse(urlObj.query);
+//         handleMoment(request, response, params);
+//         return;
+//     }
+
+//     // if (urlObj.pathname === '/') {
+//     //     console.log(urlObj.pathname);
+//     //     urlObj.pathname = 'index.html'
+//     // }
+//     getFile(urlObj.pathname)
+//         .then(data => handleFile(request, response, data))
+//         .catch(err => handleFileError(request, response, err))
+// }
 function handleFile(request, response, data) {
     response.end(data);
 }
@@ -39,5 +66,5 @@ function handleFileError(request, response, err) {
     sendStatus(request, response, 500);
 }
 function handleMoment(request, response, params) {
-    response.end(`data`);
+    response.end('data');
 }
